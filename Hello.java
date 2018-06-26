ed066cd5393e208a1b14a4c1eed6d01f880255e0
dharuvelu <40588588+dharuvelu@users.noreply.github.com>
2018-06-26 18:49:17 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Hello
+{
+public static void main(String[] arg)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=0;i<n;i++)
+{
+System.out.println("Hello");
+}
+}
+}
