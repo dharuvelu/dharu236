6d8453dfb45cf8a0ecb1bbb11cb8e1ac417e47f0
dharuvelu <40588588+dharuvelu@users.noreply.github.com>
2018-06-26 19:01:08 +0530

    Natural.java

---
+++ Natural.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Natural
+{
+public static void main(String[] arg)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.next();
+int a=0;
+for(int i=0;i<n;i++)
+{
+a=a+i;
+}
+System.out.print(a);
+}
+}
